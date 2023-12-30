c39b724d6722b850c36e10a3c420f7ece93ae462
Anushk-Pandey <anushkpandey56@gmail.com>
2023-12-30 17:56:09 +0530

    ARRAY PROGRAMS

--- ARRAY/array3.java
+++ ARRAY/array3.java
@@ -1,41 +1,40 @@
-import java.util.Scanner; 
-public class array3{ 
-    public static void main(String[] args) 
-    { 
-      int temp=0;
-
-      Scanner sc = new Scanner(System.in); 
+import java.util.Scanner;  
   
-       // Take the array size from the user 
-       System.out.println("Enter the size of the array: "); 
-       int arr_size = 0; 
-       if (sc.hasNextInt()) { 
-           arr_size = sc.nextInt(); 
-       } 
-  
-      // Initialize the array's 
-      // Size using user input 
-      int[] arr = new int[arr_size]; 
-
-      System.out.println("Enter the elements of the array: "); 
-      //taking array from user
-      for (int i = 0; i < arr_size; i++) { 
-          if (sc.hasNextInt()) { 
-              arr[i] = sc.nextInt(); 
-           } 
-      } 
-      for(int i = 0; i < arr_size; i++){
-        for(int j = i+1; i < arr_size; j++){
-          if(arr[j] < arr[i]){
-            temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-          }
-        }
-      }
-      System.out.println("Elements after sorting :");
-      for (int i = 0; i < arr_size; i++){
-          System.out.println(arr[i]);
-      }
-    }
-  }
+public class array3  
+{  
+   public static void main(String args[])  
+   {  
+       int size, i, j, temp;  
+       int arr[] = new int[50];  
+       Scanner scan = new Scanner(System.in);  
+         
+       System.out.print("Enter Array Size : ");  
+       size = scan.nextInt();  
+         
+       System.out.print("Enter Array Elements : ");  
+       for(i=0; i<size; i++)  
+       {  
+           arr[i] = scan.nextInt();  
+       }  
+         
+       System.out.print("Sorting Array using Selection Sort Technique..\n");  
+       for(i=0; i<size; i++)  
+       {  
+           for(j=i+1; j<size; j++)  
+           {  
+               if(arr[i] > arr[j])  
+               {  
+                   temp = arr[i];  
+                   arr[i] = arr[j];  
+                   arr[j] = temp;  
+               }  
+           }  
+       }  
+         
+       System.out.print("Now the Array after Sorting is :\n");  
+       for(i=0; i<size; i++)  
+       {  
+           System.out.print(arr[i]+ "  ");  
+       }  
+   }  
+}  
